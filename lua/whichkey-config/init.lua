2cf1ab8b6b2b5164467ea995554b6ef3e446df70
peter159 <peter.linyi159@gmail.com>
2021-12-12 17:59:41 +0800

    add lsp command

--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -60,16 +60,21 @@ local mappings = {
   },
 
   l = {
-    name = "LSP",
-      i =  {":LspInfo<CR>",  "Connected Language Servers"},
-      A =  {"<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>",  "Add workspace folder"},
-      R =  {"<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>",  "Remove workspace folder"},
-      l =  {"<cmd>lua print(vim.inspect(vim.lsp.buf.add_workspace_folder()))<CR>",  "List workspace folder"},
-      D =  {"<cmd>lua vim.lsp.buf.type_definition()<CR>",  "Type definition"},
-      r =  {"<cmd>lua vim.lsp.buf.rename()<CR>",  "Rename"},
-      a =  {"<cmd>lua vim.lsp.buf.code_action()<CR>",  "Code actions"},
-      e =  {"<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>",  "Show line diagnostics"},
-      q =  {"<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>",  "Show loclist"},
+    name = "lsp",
+    g = {
+      name = "goto",
+      d = {"<Cmd>lua vim.lsp.buf.definition()<CR>", "go to definition"},
+      D = {"<Cmd>lua vim.lsp.buf.declaration()<CR>", "go to declaration"},
+      i = {"<cmd>lua vim.lsp.buf.implementation()<CR>", "go to implementation"},
+      r = {"<cmd>lua vim.lsp.buf.references()<CR>", "go to reference"}
+    },
+    t = {"<cmd>lua vim.lsp.buf.type_definition()<CR>", "type definition"},
+    w = {
+      name = "workspace",
+      a = {"<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "add workspace"},
+      r = {"<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "remove workspace"},
+      l = {"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", "list workspaces"}
+    }
   },
 }
 local opts = {prefix = '<leader>'}
